b16b8cdc441ac83af405f9dca0e84cdac4b610b4
sparky9043 <sparky9043@gmail.com>
2024-07-26 20:24:08 +0900

    Create separate file for update ui function

---
+++ src/UpdateUI.js
@@ -0,0 +1,84 @@
+import './style.css';
+import { compareAsc } from 'date-fns';
+
+const UpdateUI = (target) => {
+  const display = document.querySelector(target);
+
+  const clearDisplay = (target) => {
+    while (target.firstChild) {
+      target.removeChild(target.firstChild);
+    }
+  }
+
+  const updateDisplay = (projectsList) => {
+
+    clearDisplay(display);
+
+    const sortedProjectsList = projectsList.sort((a, b) => compareAsc(a.dueDate, b.dueDate));
+
+    for (let i = 0; i < sortedProjectsList.length; i ++) {
+      const addedProject = sortedProjectsList[i];
+
+      const article = document.createElement('article');
+      const title = document.createElement('h2');
+      const description = document.createElement('p');
+      const dueDate = document.createElement('p');
+      const priority = document.createElement('p');
+      const removeBtn = document.createElement('button');
+  
+      article.classList.add('card');
+      article.dataset.id = i;
+      removeBtn.id = 'remove-btn';
+  
+      title.textContent = `Project Title: ${addedProject.title}`;
+      description.textContent = `Description: ${addedProject.description}`;
+      dueDate.textContent = `Due Date: ${addedProject.dueDate}`;
+      priority.textContent = `Priority Level: ${addedProject.priority}`;
+      removeBtn.textContent = 'Remove Project';
+
+      removeBtn.addEventListener('click', removeCard);
+
+      function removeCard() {
+        function removeFromDisplayAndList() {
+          this.remove();
+          sortedProjectsList.splice(this.dataset.id, 1);
+
+          console.log(sortedProjectsList);
+        }
+
+        function reassignID() {
+          const articles = display.querySelectorAll('article');
+
+          for (let i = this.dataset.id; i < articles.length; i++) {
+            articles[i].dataset.id = i;
+          }
+        }
+
+        removeFromDisplayAndList.call(article);
+        reassignID.call(article);
+      }
+  
+      const elements = [
+        title,
+        description,
+        dueDate,
+        priority,
+        removeBtn,
+      ];
+  
+      for (const element of elements) {
+        article.appendChild(element);
+      }
+  
+      display.appendChild(article);
+    }
+
+    console.log(sortedProjectsList);
+  }
+
+  return {
+    updateDisplay,
+  }
+}
+
+export default UpdateUI;
