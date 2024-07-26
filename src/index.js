7f126f1e24f380b8d3c73128a1ac56161bb0159d
sparky9043 <sparky9043@gmail.com>
2024-07-26 14:29:52 +0900

    Sort project list in get projects list function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
+import { compareAsc } from 'date-fns';
 import Project from './ProjectClass';
 
 const ProjectGenerator = () => {
   const projectsList = [];
 
-  const getProjectsList = () => projectsList;
+  const getProjectsList = () => projectsList.sort((a, b) => compareAsc(a.dueDate, b.dueDate));
 
   const addProjectToList = (title, description, dueDate, priority) => {
     const project = new Project(title, description, dueDate, priority);
@@ -24,12 +25,13 @@ const ProjectGenerator = () => {
   }
 }
 
-const myProject = ProjectGenerator();
+const project = ProjectGenerator();
 
-myProject.addProjectToList('hi1', 'hello', '2017-09-15', 'low');
-myProject.addProjectToList('hi2', 'hello', '2017-09-15', 'low');
-myProject.addProjectToList('hi3', 'hello', '2017-09-15', 'low');
+project.addProjectToList('Hi0', 'Hello0', '2020-12-17', 'low');
+project.addProjectToList('Hi1', 'Hello1', '2015-12-17', 'medium');
+project.addProjectToList('Hi2', 'Hello2', '2024-12-17', 'high');
+project.addProjectToList('Hi3', 'Hello3', '2018-12-17', 'low');
+project.addProjectToList('Hi4', 'Hello4', '2011-12-17', 'low');
 
-myProject.removeProjectFromList(1);
 
-console.log(myProject.getProjectsList());
+console.log(project.getProjectsList());
