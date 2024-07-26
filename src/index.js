b59df7211e8ed5ac070e0c2ca9645753f37624c4
sparky9043 <sparky9043@gmail.com>
2024-07-26 12:54:07 +0900

    Import project class from project class script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './style.css';
+import Project from './ProjectClass';
 
-console.log('hello');
-console.log("nice");
+const generateProject = (title, description, dueDate, priority) => {
+
+  const project = new Project(title, description, dueDate, priority);
+
+  console.log(project);
+}
+
+generateProject('hi', 'hello', '2017-12-12', 'high');
