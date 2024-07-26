12e989d0ad0a33373b4937815b63f1b264a14d5f
sparky9043 <sparky9043@gmail.com>
2024-07-26 13:23:57 +0900

    Revise format function for taking in argument

--- src/ProjectClass.js
+++ src/ProjectClass.js
@@ -1,10 +1,10 @@
-import { formatDate } from 'date-fns';
+import { format } from 'date-fns';
 
 class Project {
   constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
-    this.dueDate = formatDate(dueDate, "MMM-dd-yyyy");
+    this.dueDate = format(dueDate, "MMM-dd-yyyy");
     this.priority = priority;
   }
 }
