e68643e1ac8ed06edd73c2fcd47b3cf89c664405
sparky9043 <sparky9043@gmail.com>
2024-07-26 13:39:55 +0900

    Create add and remove project functions in generator

--- src/index.js
+++ src/index.js
@@ -4,18 +4,32 @@ import Project from './ProjectClass';
 const ProjectGenerator = () => {
   const projectsList = [];
 
-  const getProjectsArray = () => projectsList;
+  const getProjectsList = () => projectsList;
 
-  const makeProject = (title, description, dueDate, priority) => {
+  const addProjectToList = (title, description, dueDate, priority) => {
     const project = new Project(title, description, dueDate, priority);
     projectsList.push(project);
     return project;
   }
 
+  const removeProjectFromList = (index) => {
+    projectsList.splice(index, 1);
+    return projectsList;
+  }
+
   return {
-    getProjectsArray,
-    makeProject,
+    getProjectsList,
+    addProjectToList,
+    removeProjectFromList,
   }
 }
 
 const myProject = ProjectGenerator();
+
+myProject.addProjectToList('hi1', 'hello', '2017-09-15', 'low');
+myProject.addProjectToList('hi2', 'hello', '2017-09-15', 'low');
+myProject.addProjectToList('hi3', 'hello', '2017-09-15', 'low');
+
+myProject.removeProjectFromList(1);
+
+console.log(myProject.getProjectsList());
